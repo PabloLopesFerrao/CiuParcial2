48842ce9e1057593f41e6c9e54f748b1df57f9c1
Pablo <92339047+PabloLopesFerrao@users.noreply.github.com>
2023-07-06 10:36:05 -0300

    Parcial subido

---
+++ src/App.js
@@ -0,0 +1,138 @@
+
+import { useEffect } from 'react';
+import axios from 'axios'
+import './App.css';
+import { useState } from 'react';
+import YouTube from 'react-youtube';
+
+
+
+function App() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "8d9cef87878ee6d08a142854dfa3d717";
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
+  // endpoint para las imagenes
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+
+  // variables de estado
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  //const [selectedMovie, setSelectedMovie] = useState({})
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
+
+  // funcion para realizar la peticion get a la api
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+
+
+    setMovies(results);
+    setMovie(results[0]);
+
+    if (results.length) {
+      await fetchMovie(results[0].id);
+    }
+  };
+
+  // funcion para la peticion de un solo objeto y mostrar en reproductor de videos
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos",
+      },
+    });
+
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+    //return data
+    setMovie(data);
+  };
+
+  const selectMovie = async (movie) => {
+    // const data = await fetchMovie(movie.id)
+    // console.log(data);
+    // setSelectedMovie(movie)
+    fetchMovie(movie.id);
+
+    setMovie(movie);
+    window.scrollTo(0, 0);
+  };
+
+  // funcion para buscar peliculas
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      
+      <a href='' style={{ textDecoration: 'none' }}>
+      <div class="titulo">
+      <h2 className="text-center mt-5 mb-5">Peliculas Populares</h2>
+      </div>
+      </a>
+      
+      {/*El Buscador */}
+      <form className=" mb-4 form-group" onSubmit={searchMovies}>
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Ingrese Pelicula"
+            onChange={(e) => setSearchKey(e.target.value)}
+          />
+    <div className="input-group-append">
+      <button className="btn btn-primary" type="submit">
+        Buscar
+      </button>
+    </div>
+  </div>
+</form>
+
+     
+    
+      {/* contenedor para mostrar los posters y las peliculas en la peticion a la api */}
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              className="col-md-4 mb-3"
+              onClick={() => selectMovie(movie)}
+            >
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
